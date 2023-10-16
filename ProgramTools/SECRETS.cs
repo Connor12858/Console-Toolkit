28346017a1d1da9278d77f04bdb8327ff96abe3e
Connor Godden <65793914+Connor12858@users.noreply.github.com>
2023-10-15 23:39:18 -0400

    Create SECRETS.cs

---
+++ ProgramTools/SECRETS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Toolkit.ProgramTools
+{
+    public class SECRETS
+    {
+        public static string IPINFO_TOKEN = "YOUR TOKEN HERE";
+    }
+}
